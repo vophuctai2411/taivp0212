80dfcb8c4d22326eb35fbfcfa1e1d7558886a79f
vophuctai2411 <vophuctai2411@gmail.com>
2022-12-01 23:47:59 +0700

    change 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./containers/header";
+import WelcomeSection from "./containers/welcomeSection";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          TEST DEPLOYMENT REACT version 02
-        </a>
-      </header>
+    <div className="background">
+      <Header />
+      <WelcomeSection />
     </div>
   );
 }
